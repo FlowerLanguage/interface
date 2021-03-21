3d93d90751324b31042231bc7866e124998d1f57
FlowersLanguage <1798419176@qq.com>
2021-03-21 13:23:55 +0800

    修改了之前model中的错误，完善了views类的继承

--- movie/models.py
+++ movie/models.py
@@ -6,7 +6,7 @@ class Movie2011(models.Model):
     name = models.CharField(max_length=20)  # 电影名
     box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房
     avg_avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价
-    avg_players = models.DecimalField(max_length=5, decimal_places=0)  # 场均人次
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
     url = models.URLField()  # 电影详情页
 
     class Meta:
--- movie/urls.py
+++ movie/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from movie import views
 
-patterns = [
+urlpatterns = [
     url(r'^movie/2011/$', views.Movie2011ListView.as_view()),
     url(r'^movie/2011/(?P<pk>\d+)/$', views.Movie2011DetailView.as_view()),
+
+
+
 ]
