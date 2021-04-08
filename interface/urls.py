647f45893f74c5346f4a666dd150f8e262a84fa8
FlowersLanguage <1798419176@qq.com>
2021-04-08 12:47:48 +0800

    增添了接口文档路由

--- interface/urls.py
+++ interface/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title='Api Document')),
+
     path('', include('lightnovel.urls')),
     path('', include('foreign_currency.urls')),
     path('', include('movie.urls')),
     path('', include('house_price.urls')),
-    path('',include('account.urls')),
+    path('', include('account.urls')),
 ]
