b53d02e1955c55cf4f02dab7091bb04a64f492ae
FlowersLanguage <1798419176@qq.com>
2021-04-08 11:42:08 +0800

    后期实现的时候，发现models中不能出现继承(否则所有数据只能出现在父类的表中)，因此还是改为最原先的方式，有多少年份(模型)就建立多少类

--- movie/models.py
+++ movie/models.py
@@ -2,69 +2,134 @@ from django.db import models
 
 
 # Create your models here.
-class Movie(models.Model):  # 模板类,后面所有电影类与他继承
-    name = models.CharField(max_length=20)  # 电影名
-    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房
-    avg_avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价
+
+class MovieWhole(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
     avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
     url = models.URLField()  # 电影详情页
 
-
-class MovieWhole(Movie):
     class Meta:
         db_table = 'movie_whole'
 
 
-class Movie2021(Movie):
+class Movie2021(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2021'
 
 
-class Movie2020(Movie):
+class Movie2020(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2020'
 
 
-class Movie2019(Movie):
+class Movie2019(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2019'
 
 
-class Movie2018(Movie):
+class Movie2018(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2018'
 
 
-class Movie2017(Movie):
+class Movie2017(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2017'
 
 
-class Movie2016(Movie):
+class Movie2016(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2016'
 
 
-class Movie2015(Movie):
+class Movie2015(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2015'
 
 
-class Movie2014(Movie):
+class Movie2014(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2014'
 
 
-class Movie2013(Movie):
+class Movie2013(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2013'
 
 
-class Movie2012(Movie):
+class Movie2012(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2012'
 
 
-class Movie2011(Movie):
+class Movie2011(models.Model):
+    name = models.CharField(max_length=50)  # 电影名
+    box = models.DecimalField(max_digits=10, decimal_places=2)  # 票房(万元)
+    avg_fare = models.DecimalField(max_digits=5, decimal_places=2)  # 平均票价(元)
+    avg_players = models.DecimalField(max_digits=5, decimal_places=0)  # 场均人次
+    url = models.URLField()  # 电影详情页
+
     class Meta:
         db_table = 'movie_2011'
