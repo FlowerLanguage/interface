91b5816aed2f78b9bc8e3924d4da51557eca6bce
FlowersLanguage <1798419176@qq.com>
2021-04-23 14:01:53 +0800

    删除了原login.vue和home.vue上面的文件夹，直接放views下面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,20 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home/Home.vue'
-import Login from '../views/login/login.vue'
+import Login from '../views/login.vue'
+import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
   },
 ]
 
